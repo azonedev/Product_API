d7f9678b6c5a139aa37fbab3468ce39879f2621e
azonedev <abdullah914115@gmail.com>
2022-01-22 21:06:23 +0600

    Created migration [database schema], model file for Products

---
+++ database/migrations/2022_01_22_145651_create_products_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id('product_id');
+            $table->unsignedBigInteger('category_id');
+            $table->string('title');
+            $table->longText('description');
+            $table->decimal('price');
+            $table->timestamps();
+            $table->foreign('category_id')->references('category_id')->on('categories')->onDelete('cascade');
+            $table->index(['title']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
